b593da4168f1ff631f16cc55fa21b2db175a0639
EC2 Default User <ec2-user@ip-172-31-39-50.ap-northeast-1.compute.internal>
2025-03-15 15:47:53 +0000

    Add Login

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_customer, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -31,5 +32,27 @@ class Public::SessionsController < Devise::SessionsController
 
   def after_sign_out_path_for(resource)
     root_path
+  end
+
+  def reject_customer
+    @customer = Customer.find_by(email: params[:customer][:email])
+  
+    if @customer
+      if @customer.valid_password?(params[:customer][:password])
+        if @customer.active_for_authentication?
+          # 顧客がアクティブな場合はそのままログイン
+          # ここでログイン処理を行うなど
+        else
+          flash[:error] = "退会済みです。再度新規登録をしてご利用ください。"
+          redirect_to new_customer_registration_path and return
+        end
+      else
+        flash[:error] = "パスワードが間違っています。"
+        redirect_to new_customer_session_path
+      end
+    else
+      flash[:error] = "該当するユーザーが見つかりません。"
+      redirect_to new_customer_session_path
+    end
   end
 end
