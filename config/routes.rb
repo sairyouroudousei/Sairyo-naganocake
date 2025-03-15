f483b52b8bc257307c44498ad6638f0fcbbe6a93
EC2 Default User <ec2-user@ip-172-31-39-50.ap-northeast-1.compute.internal>
2025-03-15 01:03:08 +0000

    Update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,80 +1,46 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'order_details/update'
-  end
-  namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'geres/index'
-    get 'geres/create'
-    get 'geres/edit'
-    get 'geres/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/thanks'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/unsubscribe'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'home/top'
-    get 'home/about'
-  end
-# 顧客用
-# URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
 
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-end
+    # customer側ルーティング
+  devise_for :customers, controllers: {
+    sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
+    registrations: 'customers/registrations'
+  }
+
+  scope module: 'customers' do
+    root 'items#top'
+    resources :items, only: [:show, :index]
+    get 'about' => 'items#about'
+  end
+
+  namespace :customers do
+    resources :genres, only: [:show]
+    patch 'customers/withdraw' => 'customers#withdraw', as: 'customers_withdraw'
+    get 'show' => 'customers#show'
+    get 'customers/edit' => 'customers#edit'
+    patch 'update' => 'customers#update'
+    get 'quit' => 'customers#quit'
+    get 'orders/about' => 'orders#about', as: 'orders_about'
+    get 'orders/complete' => 'orders#complete'
+    resources :orders, only: [:create, :new, :index, :show]
+    resources :cart_items, only: [:index, :create, :update, :destroy]
+    delete 'cart_items' => 'cart_items#all_destroy', as: 'all_destroy'
+    resources :shipping_addresses, only: [:index, :create, :destroy, :edit, :update]
+  end
+
+  # admin側ルーティング
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+
+  namespace :admins do
+    root :to => 'top#top'
+    resources :customers, only: [:index, :edit, :update, :show]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:show, :index, :new, :create, :edit, :update]
+    resources :orders, only: [:index, :show, :update]
+    resources :order_details, only: [:update]
+  end
+  
