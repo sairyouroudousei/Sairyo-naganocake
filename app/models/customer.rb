f4530b5ea1e3fcdfbc9863c820b012f86dc2a141
EC2 Default User <ec2-user@ip-172-31-39-50.ap-northeast-1.compute.internal>
2025-03-15 01:33:58 +0000

    Update models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,5 +4,22 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :cart_items, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  has_many :addresses, dependent: :destroy
+
+  def active_for_authentication?
+    super && (self.is_customer_status == true)
+  end
+
+  validates :last_name,  presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana,  presence: true
+  validates :first_name_kana, presence: true
+  validates :phone_number, presence: true
+  validates :postal_code,  presence: true
+  validates :address, presence: true
+
+
   
 end
