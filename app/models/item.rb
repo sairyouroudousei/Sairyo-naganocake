8c0c22d83cd78560c4b22144ccacdc0324c2994c
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-03-18 08:24:45 +0000

    [fix]メソッドエラー

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,8 @@ class Item < ApplicationRecord
   has_many :cart_items
   has_many :order_details
 
-  attachment :image
+  #attachment :image 
+  has_one_attached :image
   validates :name, {presence: true}
   validates :introduction, {presence: true}
   validates :price, {presence: true}
