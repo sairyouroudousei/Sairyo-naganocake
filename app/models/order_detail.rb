fb0d076b971f50487de3ec5c4cdb525b62535a3b
EC2 Default User <ec2-user@ip-172-31-39-50.ap-northeast-1.compute.internal>
2025-03-21 22:25:39 +0000

    Change enum ja

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,5 +2,5 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  enum making_status: {"着手不可":0,"製作待ち":1,"製作中":2,"製作完了":3,}
+  enum making_status: {unavoidable: 0, awaiting_manufacture: 1, in_production: 2, completion_of_production: 3,}
 end
