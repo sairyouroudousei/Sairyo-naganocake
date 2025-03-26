0b9296e1c20747b825ac9dcfb81e6cd68358f3e8
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-03-26 02:43:34 +0000

    [fix]サイズとページネーション修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,6 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page]).per(2) # ← 1ページに2件ずつ表示
+    @items = Item.page(params[:page]).per(5) # ← 1ページに5件ずつ表示
   end
 
   def show
